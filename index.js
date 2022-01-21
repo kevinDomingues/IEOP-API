f3fcfc8c54b78c72ecc838f7b54b5f10441a9bad
kevinDomingues <77273627+kevinDomingues@users.noreply.github.com>
2022-01-21 23:50:02 +0000

    Major changes

--- index.js
+++ index.js
@@ -78,119 +78,6 @@ app.get("/getItemsForSale", async (req, res) => {
 })
 
 app.post("/criarFatura", async (req, res) => {
-    console.log(req.body);
-
-    let item = req.body.KeyCarro;
-    let buyerCustomerParty = req.body.Cliente;
-	let email = req.body.Email;
-	//data e converter para rfc3339
-	let dateTime = new Date();
-	let dateTimeFormatted = dateTime.toISOString();
-
-    let documentLines = {
-        salesItem: item,
-        quantity: 1,
-        warehouse: "01"
-    }
-
-    let body = {
-        "company": "SI",
-        "documentType": "FA",
-        "buyerCustomerParty": buyerCustomerParty,
-        "emailTo": email,
-        "documentDate": dateTimeFormatted,
-        "documentLines": [documentLines]
-    }
-
-    const options = {
-        'method': 'POST',
-        'url': `${PRIMAVERA_BASE_URL}/billing/invoices`,
-        'headers': {
-            'Authorization': `Bearer ${await getToken()}`,
-        },
-        'body': body,
-        json: true
-    }
-
-    request(options, async function (err, response, body) {
-        if (err){
-            res.status(400).json({
-                status: false,
-                message: "Dados inválidos"
-            });
-            return;
-        }
-        if (body) {
-            res.status(201).json({
-                status: true,
-                message: body
-            });
-        }
-    })
-})
-
-app.post("/acertarStock", async (req, res) => {
-    console.log(req.body);
-
-    let item = req.body.KeyCarro;
-    let buyerCustomerParty = req.body.Cliente;
-	let email = req.body.Email;
-	//data e converter para rfc3339
-	let dateTime = new Date();
-	let dateTimeFormatted = dateTime.toISOString();
-
-    let documentLines = {
-        salesItem: item,
-        quantity: 1,
-        warehouse: "01"
-    }
-
-    let body = {
-        "company": "SI",
-        "documentType": "FA",
-        "buyerCustomerParty": buyerCustomerParty,
-        "emailTo": email,
-        "documentDate": dateTimeFormatted,
-        "documentLines": [documentLines]
-    }
-
-    const options = {
-        'method': 'POST',
-        'url': `${PRIMAVERA_BASE_URL}/billing/invoices`,
-        'headers': {
-            'Authorization': `Bearer ${await getToken()}`,
-        },
-        'body': body,
-        json: true
-    }
-
-    
-
-    // const filename = `FATURA_${moment().unix()}.pdf`;
-    // const filepath = `./public/${filename}`;
-    // let file = fs.createWriteStream(filepath);
-    // let print = `${PRIMAVERA_BASE_URL}/billing/invoices/${body}/print`;
-
-    request(options, async function (err, response, body) {
-        if (err){
-            res.status(400).json({
-                status: false,
-                message: "Dados inválidos"
-            });
-            return;
-        }
-        if (body) {
-            res.status(201).json({
-                status: true,
-                message: body
-            });
-        }
-    })
-})
-
-app.post("/criarEncomenda", async (req, res) => { 
-    let buyerCustomerParty = '';
-    let Email = '';
     if (typeof req.body.KeyCarro === "undefined") {
 		res.status(400).json({
 			status: false,
@@ -199,27 +86,170 @@ app.post("/criarEncomenda", async (req, res) => {
 		return;
 	}
 	if (typeof req.body.Cliente === "undefined") {
-
-        buyerCustomerParty = 'INDIF'
-		/*res.status(400).json({
+		res.status(400).json({
 			status: false,
 			message: "buyerCustomerParty inválido: " + req.body.Cliente
 		});
-		return; */
+		return;
 	}
-	if (typeof req.body.Email === "undefined") {
-		/*res.status(400).json({
-			status: false,
-			message: "email inválido: " + req.body.Email
-		});
-		return; */
+    let item = req.body.KeyCarro;
+    let buyerCustomerParty = req.body.Cliente;
+	let email = req.body.Email;
+	//data e converter para rfc3339
+	let dateTime = new Date();
+	let dateTimeFormatted = dateTime.toISOString();
 
-        Email="projeto3ipvc@.pt"
+    let documentLines = {
+        salesItem: item,
+        quantity: 1,
+        warehouse: "02"
+    }
+
+    let body = {
+        "company": "SI",
+        "documentType": "FA",
+        "buyerCustomerParty": buyerCustomerParty,
+        "emailTo": email,
+        "documentDate": dateTimeFormatted,
+        "documentLines": [documentLines]
+    }
+
+    const options = {
+        'method': 'POST',
+        'url': `${PRIMAVERA_BASE_URL}/billing/invoices`,
+        'headers': {
+            'Authorization': `Bearer ${await getToken()}`,
+        },
+        'body': body,
+        json: true
+    }
+
+    request(options, async function (err, response, body) {
+        if (err){
+            res.status(400).json({
+                status: false,
+                message: "Dados inválidos"
+            });
+            return;
+        }
+        if (body) {
+            res.status(201).json({
+                status: true,
+                message: body
+            });
+        }
+    })
+})
+
+app.post("/criarCliente", async (req, res) => {
+    let nome = req.body.Nome;
+    let nif = req.body.Contribuinte;
+	let email = req.body.Email;
+    let morada = req.body.Morada;
+    let tel = req.body.Telemovel;
+
+    let body = {
+        "name": nome,
+        "electronicMail": email,
+        "companyTaxID": nif,
+        "streetName": morada,
+        "mobile": tel,
+		"culture": "PT-PT",
+		"country": "PT",
+		"currency": "EUR"
+    }
+
+    const options = {
+        'method': 'GET',
+        'url': `${PRIMAVERA_BASE_URL}/salescore/customerparties/odata`,
+        'headers': {
+            'Authorization': `Bearer ${await getToken()}`,
+        },
+        json: true
+    }
+
+    request(options, async function (err, response) {
+        if (err){
+            res.status(400).json({
+                status: false,
+                message: "Erro"
+            });
+            return;
+        }
+        let exists = 0;
+        let partyKey = 0;
+
+        let search = {
+            "Username": nome
+        };
+        if (response.body) {
+            for(let client of response.body.items) {
+                if(client.name == search.Username){
+                    exists = 1;
+                    partyKey = client.partyKey;
+                }
+            }
+
+            if(exists == 0) {
+                let createOptions = {
+                    'method': 'POST',
+                    'url': `${PRIMAVERA_BASE_URL}/salescore/customerparties`,
+                    'headers': {
+                        'Authorization': `Bearer ${await getToken()}`,
+                    },
+                    json: true,
+                    body: body
+                }
+                request(createOptions, async function (err2, response2) {
+
+                    let createOptions2 = {
+                        'method': 'GET',
+                        'url': `${PRIMAVERA_BASE_URL}/salescore/customerparties/odata`,
+                        'headers': {
+                            'Authorization': `Bearer ${await getToken()}`,
+                        },
+                        json: true,
+                    }
+                    await request(createOptions2, async function (err3, response3){
+                        if(response3.body) {
+                            for(let client of response3.body.items){
+                                if(client.name == search.Username){
+                                    partyKey = client.partyKey;
+                                }
+                            }
+                        }
+                        res.status(200).json({
+                            "userKey": partyKey
+                        });
+                    })
+                })
+            } else {
+                res.status(200).json({
+                    "userKey": partyKey
+                });
+            }
+        }
+    })
+})
+
+app.post("/criarEncomenda", async (req, res) => { 
+    if (typeof req.body.KeyCarro === "undefined") {
+		res.status(400).json({
+			status: false,
+			message: "salesItem inválido: " + req.body.KeyCarro
+		});
+		return;
+	}
+	if (typeof req.body.Cliente === "undefined") {
+		res.status(400).json({
+			status: false,
+			message: "buyerCustomerParty inválido: " + req.body.Cliente
+		});
+		return;
 	}
 
     let salesItem = req.body.KeyCarro;
-   /* let buyerCustomerParty = req.body.Cliente;
-	let email = req.body.Email; */
+    let buyerCustomerParty = req.body.Cliente;
 	//data e converter para rfc3339
 	let dateTime = new Date();
 	let dateTimeFormatted = dateTime.toISOString();
